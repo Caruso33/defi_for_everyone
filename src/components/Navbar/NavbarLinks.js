515fc7172a09ae71cd29ca21ea78617c7db87354
Tobias Leinss <caruso33@web.de>
2022-08-18 10:57:54 +0200

    Make moralis work

--- src/components/Navbar/NavbarLinks.js
+++ src/components/Navbar/NavbarLinks.js
@@ -1,10 +1,14 @@
-import { Flex } from "@chakra-ui/react"
+import { Flex, Icon, Tooltip } from "@chakra-ui/react"
 import SidebarResponsive from "components/Sidebar/SidebarResponsive"
 import PropTypes from "prop-types"
+import { FaWallet } from "react-icons/fa"
+import { useMoralis } from "react-moralis"
 import routes from "routes.js"
 
 export default function HeaderLinks(props) {
   const { variant, children, fixed, secondary, onOpen, ...rest } = props
+
+  const { enableWeb3, isWeb3Enabled, deactivateWeb3, account } = useMoralis()
 
   return (
     <Flex
@@ -20,7 +24,19 @@ export default function HeaderLinks(props) {
         // logo={logo}
         {...rest}
       />
-      Sign In
+
+      <Tooltip placement="left" label={account} shouldWrapChildren>
+        <Icon
+          onClick={isWeb3Enabled ? deactivateWeb3 : enableWeb3}
+          cursor="pointer"
+          me="16px"
+          ms={{ base: "16px", xl: "0px" }}
+          color={isWeb3Enabled ? "teal.300" : "gray.500"}
+          w="18px"
+          h="18px"
+          as={FaWallet}
+        />
+      </Tooltip>
     </Flex>
   )
 }
