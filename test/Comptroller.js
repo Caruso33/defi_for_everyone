942ab516a016cac2065e72e5f871673e863513c8
Tobias Leinss <caruso33@web.de>
2022-08-25 15:58:40 +0200

    Change var naming

--- test/Comptroller.js
+++ test/Comptroller.js
@@ -2,7 +2,6 @@ const IERC20 = require("../artifacts/contracts/IERC20.sol/IERC20.json")
 const ComptrollerLib = require("../artifacts/contracts/ComptrollerLib.sol/ComptrollerLib.json")
 const IVault = require("../artifacts/contracts/IVault.sol/IVault.json")
 const { expect } = require("chai")
-// const transferUsdc = require("../scripts/transferUSDC")
 const hre = require("hardhat")
 
 const { ethers } = hre
@@ -10,7 +9,7 @@ const { ethers } = hre
 describe("Comptroller", function () {
   const usdcAddressProxy = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
   // const usdcOwner = "0xfcb19e6a322b27c06842a71e8c725399f049ae3a"
-  const vaultAddress = "0x23d3285bfe4fd42965a821f3aecf084f5bd40ef4"
+  const vaultAddress = "0xded69068a94776a23f5bdafc6b4c6894bc88e82c"
   let comptrollerAddress = ""
 
   let signer
@@ -37,12 +36,6 @@ describe("Comptroller", function () {
     comptrollerProxy = comptrollerLogic.attach(comptrollerAddress)
   })
 
-  describe("Deployment", function () {
-    it("can do anything", async () => {
-      expect(true).to.equal(true)
-    })
-  })
-
   describe("buyShares", function () {
     it("can transfer shares", async () => {
       const address = await signer.getAddress()
@@ -52,21 +45,19 @@ describe("Comptroller", function () {
         ethers.utils.formatEther((await ethers.provider.getBalance(address)).toString())
       )
 
-      let deployerBalance = await usdcContractProxy.balanceOf(address)
-      console.log("usdc before", deployerBalance.toString())
+      let usdcBalance = await usdcContractProxy.balanceOf(address)
+      console.log("usdc before", usdcBalance.toString())
 
       const minSharesQuantities = 1
-      await comptrollerProxy.buyShares(
-        [address],
-        [deployerBalance.toNumber()],
-        [minSharesQuantities]
-      )
+      await comptrollerProxy.buyShares([address], [usdcBalance.toNumber()], [minSharesQuantities])
 
-      deployerBalance = await usdcContractProxy.balanceOf(address)
-      console.log("usdc after", deployerBalance.toString())
+      const usdcBalanceAfter = await usdcContractProxy.balanceOf(address)
+      console.log("usdc after", usdcBalance.toString())
 
-      deployerBalance = await vaultProxy.balanceOf(address)
-      console.log("vault token", deployerBalance.toString())
+      const vaultBalance = await vaultProxy.balanceOf(address)
+      console.log("vault token", vaultBalance.toString())
+
+      expect(usdcBalanceAfter.toString()).to.equal("0")
     })
   })
 })
